4b862c39d4dd86d9550e80de60f75de75d4514da
Henrique <henryqueoli@hotmail.com>
2023-05-29 16:25:55 -0300

    feat: v-dialog

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
-import Logo from './assets/logo.svg?raw'
+import { ref } from 'vue'
+import VDialog from './components/VDialog.vue'
+
+const show = ref(false)
 </script>
 <template>
   <div class="flex h-screen w-screen items-center justify-center bg-gray-900">
     <div class="min-h-[300px] min-w-[500px] rounded bg-white p-5 text-center">
       <div class="flex h-[300px] w-full flex-col items-center justify-center">
-        <div v-html="Logo" class="mx-auto h-32 w-32" />
-
-        <div class="text-2xl">Sidekick-coder template</div>
+        <button @click="show = true" class="bg-teal-500 p-2 text-white">Show dialog</button>
+        <v-dialog v-model="show"> Hello word </v-dialog>
       </div>
     </div>
   </div>
